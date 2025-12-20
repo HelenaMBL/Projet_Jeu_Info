5e07941abf4c24f7bbbd34cf652386ed97b98cca
Hélèna MB <helena.meyerblanco@edu.ece.fr>
2025-12-20 12:08:23 +0100

    Nettoyage du build.bat

--- build.bat
+++ build.bat
@@ -1,16 +1,13 @@
 cls
-rem del *.o *.exe
-rem gcc -c code_projet_info.c -o code_projet_info.o
-rem gcc -c affichage_console.c -o affichage_console.o
-rem gcc  code_projet_info.o affichage_console.o -o code_projet_info.exe
-rem del *.exe
+
 mingw32-make CandyCrush.exe
-rem gcc  code_projet_info.o affichage_console.o -o code_projet_info.exe
-rem code_projet_info.exe
+
 if ERRORLEVEL 1 (
     exit /b 1
-)
+) 
+chcp 65001
 CandyCrush.exe 
+
 
  
 
