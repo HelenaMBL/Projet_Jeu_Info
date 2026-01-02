e626721b8a1fbfd7c7640d463160f3c7ceb92715
Hélèna MB <helena.meyerblanco@edu.ece.fr>
2026-01-02 14:36:47 +0100

    Correction de bug du swap

--- Jeu.c
+++ Jeu.c
@@ -193,19 +193,11 @@ int permutationValide(int grille[LIGNES][COLONNES], int l1, int c1, int l2, int 
 
     Partie tempPartie = {0};
 
-    int valide =
-        detecterSuites4(copie, &tempPartie) ||
-        detecterSuites6(copie, &tempPartie) ||
-        detecterCroix(copie, &tempPartie) ||
-        detecterCarre(copie, &tempPartie);
+    temp = grille[l1][c1];
+    grille[l1][c1] = grille[l2][c2];
+    grille[l2][c2] = temp;
 
-    if(valide) {
-        temp = grille[l1][c1];
-        grille[l1][c1] = grille[l2][c2];
-        grille[l2][c2] = temp;
-    }
-
-    return valide;
+    return 1;
 }
 
 // Gestion de la grille au cours de la partie 
